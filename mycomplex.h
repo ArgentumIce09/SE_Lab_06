266f7fa183a2d20b9bc10e116933f2b83f6903d9
Andrey Shekin <shekin_andrei@mail.ru>
2023-01-17 12:36:12 +0500

    Исправлен листинг файла  mycomplex.h

--- mycomplex.h
+++ mycomplex.h
@@ -1,39 +1,46 @@
+/**
+\file mycomplex.h
+\brief Заголовочный файл класса Complex
+*/
+
 #ifndef _MY_COMPLEX_H_
 #define _MY_COMPLEX_H_
+
 #include <iostream>
+
 using namespace std;
+
+/**
+\brief Класс комплексного числа
+*/
+
 class Complex
-{double Re;
-	double Im; 
-	public:
-Complex(double aRe=0, double aIm=0);
-Complex(const Complex&);
-~Complex(    );
-void Set(double aRe, double aIm=0);
-operator double();
- double abs();
- friend istream& operator>>(   istream&,Complex&) ; 
+{
+/**
+\brief Действительная чать
+*/
+    double Re;
+/**
+\brief Минимая чать
+*/
+    double Im;
 
+    public:
+    Complex(double aRe = 0, double aIm = 0);
+    Complex(const Complex &);
+    Complex &operator = (const Complex&);
+    Complex &operator = (const double&);
+    Complex &operator += (const Complex&);
+    Complex &operator += (const double&);
+    Complex &operator -= (const Complex&);
+    Complex &operator -= (const double&);
+    Complex &operator *= (const Complex&);
+    Complex &operator *= (const double&);
+    Complex &operator /= (const Complex&);
+    Complex &operator /= (const double&);
 
-
-friend ostream& operator<< (ostream&, Complex & );
-
-Complex operator+ (const Complex&);
-Complex operator- (const Complex&);
-Complex operator+ (const double&);
-friend 		Complex operator+ (const double&, const Complex&);
-Complex operator- (const double&);
-friend Complex operator- (const 	double&, const Complex&);
-Complex operator* (const Complex&);
-Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
-
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+    void Set(double aRe, double aIm);
+    operator double();
+    double abs();
+}
 #endif
